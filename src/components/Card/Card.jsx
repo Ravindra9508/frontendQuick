6cee58ac5c83cbe2ed553bbeb3cdbf4705453a25
Ravindra9508 <84035081+Ravindra9508@users.noreply.github.com>
2023-11-03 02:01:51 +0530

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,14 +3,13 @@ import './Card.css';
 import { FaTimes, FaUserCircle } from 'react-icons/fa';
 import { FcRegisteredTrademark, FcBusinessman } from "react-icons/fc";
 import StatusIconGenerator from '../Header/StatusIconGenerator';
-import UserIcon from './UserIcon';
+import PriorityIconGenerator from '../Header/PriorityIconGenerator';
+import UserIcon from './UserIcon1';
 
-const users = [
-    'Anoop Sharma',
-];
 
-const Card = ({ id, title, tag, user, status }) => {
-    console.log(user);
+
+const Card = ({ id, title, tag, user, status,priority }) => {
+    console.log(priority);
     return (
         <div className="cardContainer flex-gap-10" style={{ gap: '5px' }}>
             <div className="cardHeading flex-sb ">
@@ -40,7 +39,9 @@ const Card = ({ id, title, tag, user, status }) => {
                 </div>
             </div>
             <div className="cardTags">
-                <div className="tags color-grey"> ... </div>
+                <div className="tags color-grey"> 
+                <PriorityIconGenerator priority={priority} />
+                ... </div>
                 {
                     tag?.map((elem, index) => {
                         return <div key={index} className="tags color-grey"> <span ><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 256 256" class="icon" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg" style={{ color: "gray" }}><path d="M232,128A104,104,0,1,1,128,24,104.13,104.13,0,0,1,232,128Z"></path></svg></span> {elem}</div>
